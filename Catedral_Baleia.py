10bc33834b0586356e7c4aa84ab866ec2066f71e
Carlos Alexandre <113312099+CarlosAlexandre197@users.noreply.github.com>
2023-05-14 22:41:19 -0300

    Atualizar o Catedral_Baleia.py

--- Catedral_Baleia.py
+++ Catedral_Baleia.py
@@ -2,7 +2,7 @@ print('Cadastro de Igrejas do Campo da Catedral Baleia CONAMAD!')
 id = int(input('Digite o ID da Igreja: '))
 regiao = input('Qual é a região da área da igreja? ')
 endereco = input('Digite o endereço e a cidade da sua igreja: ')
-nome = input('Qual é o nome da igreja? ')
+nome = input('Qual é o nome da sua igreja? ')
 pastor = input('Qual é o Pastor(a) da igreja? ')
 
 import sqlite3
